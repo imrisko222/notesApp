0756567d1d7847e6b5cafac1c2da7adb2923dbde
Roman Klaška <aksalk@gmail.com>
2025-07-31 15:30:49 +0200

    dokoncena Header komponenta, zacata NotesPage.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,21 @@
 import React from "react";
 import Input from "../Input/Input";
 import SettingsIcon from "../icons/setting.svg?react";
+import SearchIcon from "../icons/search.svg?react";
 import styles from "./Header.module.css";
 
 const Header = () => {
   return (
     <div className={styles.headerContainer}>
       <h3>All Notes</h3>
-      <Input
-        type="text"
-        placeholderText="Search by title, content, or tags... "
-      />
-      <SettingsIcon />
+      <div className={styles.rightSection}>
+        <Input
+          type="text"
+          placeholderText="Search by title, content, or tags... "
+          startIcon={<SearchIcon />}
+        />
+        <SettingsIcon />
+      </div>
     </div>
   );
 };
--- src/pages/NotesPage.jsx
+++ src/pages/NotesPage.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import Header from "../components/Header/Header";
+import Navigation from "../components/Sidebar/Navigation";
+import styles from "./NotesPage.module.css";
 
 const NotesPage = () => {
   return (
-    <div>
+    <div className={styles.layout}>
+      <Navigation />
       <Header />
     </div>
   );
