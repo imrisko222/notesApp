7926561a9e1e932b8f903477dd4874e74cd77c8b
Roman Klaška <aksalk@gmail.com>
2025-08-01 14:45:12 +0200

    zacaty dizajn pre hlavny page

---
+++ src/components/Sidebar/NoteListPanel.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Button from "../Buttons/Button";
+import NoteList from "./NoteList";
+import PlusIcon from "../icons/plus.svg?react";
+import styles from "./Sidebar.module.css";
+
+const NoteListPanel = () => {
+  return (
+    <div className={styles.allListContainer}>
+      <Button icon={<PlusIcon />}>Create New Note</Button>
+      <NoteList />
+    </div>
+  );
+};
+
+export default NoteListPanel;
--- src/pages/NotesPage.jsx
+++ src/pages/NotesPage.jsx
@@ -1,13 +1,29 @@
 import React from "react";
+// components
 import Header from "../components/Header/Header";
 import Navigation from "../components/Sidebar/Navigation";
+import NoteListPanel from "../components/Sidebar/NoteListPanel";
+// styles
 import styles from "./NotesPage.module.css";
 
 const NotesPage = () => {
   return (
     <div className={styles.layout}>
-      <Navigation />
-      <Header />
+      <aside className={styles.columnA}>
+        <Navigation />
+      </aside>
+      <header className={styles.header}>
+        <Header />
+      </header>
+      <section className={`${styles.coulmnB} ${styles.allListContainer}`}>
+        <NoteListPanel />
+      </section>
+      <main className={styles.columnC}>
+        <h3>nieco</h3>
+      </main>
+      <div>
+        <h3>daco</h3>
+      </div>
     </div>
   );
 };
